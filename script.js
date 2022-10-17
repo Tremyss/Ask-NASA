5050b555a4edadfe654f4655e6369537bb13bdc0
Tremyss <tremyss1@gmail.com>
2022-10-18 01:15:34 +0200

    fetch data from nasa server

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('Hello NASA');
+
+const apodData = async () => {
+    let apodResponse = fetch ("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    
+    let apodData = await (await apodResponse).json();
+    console.log(apodData);
+    let dataOutput = document.querySelector(".container");
+    let todaysDate = document.createElement("p");
+    todaysDate.setAttribute("id", "currentDate");
+    todaysDate.innerText = apodData.date
+    
+  
+}
+apodData();
